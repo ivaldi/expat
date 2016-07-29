b4e78e53904d6e1be8a8c5b692c7fb1bdb54f2b0
Frank Groeneveld <frank@ivaldi.nl>
2016-07-29 14:46:29 +0200

    Save updated translation

--- app/controllers/expat/translations_controller.rb
+++ app/controllers/expat/translations_controller.rb
@@ -8,17 +8,27 @@ module Expat
     end
 
     def edit
+      @translation = @translations[params[:id]]
     end
 
     def update
+      @translations[params[:id]] = params[:value]
+      redirect_to locale_translations_path(@locale)
+      save_translations
     end
 
     protected
 
     def load_translations
-      path = "#{Rails.root}/config/locales/#{params[:locale_id]}.yml"
+      @locale = params[:locale_id]
+      path = "#{Rails.root}/config/locales/#{@locale}.yml"
       yml = YAML.load_file path
-      @translations = iterate yml[params[:locale_id]], ''
+      @translations = iterate yml[@locale], ''
+    end
+
+    def save_translations
+      path = "#{Rails.root}/config/locales/#{@locale}.yml"
+      File.write path, { @locale => @translations }.to_yaml
     end
 
     def iterate node, parent, result = {}
